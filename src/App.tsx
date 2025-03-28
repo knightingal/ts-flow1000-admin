1fc90184d2c24e58e97183b2fda5913f7a3db2f7
knightingal <yujianming1985@hotmail.com>
2025-03-28 22:22:31 +0800

    add albumIndexPage

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 
 import IndexPage from "@/pages/index";
 import DocsPage from "@/pages/docs";
+import AlbumIndexPage from "@/pages/albumIndex";
 
 function App() {
   return (
     <Routes>
       <Route element={<IndexPage />} path="/" />
       <Route element={<DocsPage />} path="/docs" />
+      <Route element={<AlbumIndexPage />} path="/albumIndex" />
     </Routes>
   );
 }
---
+++ src/pages/albumIndex.tsx
@@ -0,0 +1,3 @@
+export default function AlbumIndexPage() {
+  return <h1>AlbumnIndexPage</h1>;
+}
