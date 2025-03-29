8c7525d9204e704583b7f0f9c53158d2f3a73fd7
knightingal <yujianming1985@hotmail.com>
2025-03-29 21:01:22 +0800

    create `PicDetail` struct

---
+++ src/store.ts
@@ -0,0 +1,11 @@
+interface PicDetail {
+  sectionIndex: number;
+  name: string;
+  cover: string;
+  index: number;
+  coverWidth: number;
+  coverHeight: number;
+  expanded: boolean;
+  album: string;
+  clientStatus: string;
+}
