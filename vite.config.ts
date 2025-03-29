4f03fca4c0c8e55334b866d02bba5a99b6ee9e7d
knightingal <yujianming1985@hotmail.com>
2025-03-29 19:49:42 +0800

    config proxy server

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,11 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    proxy: {
+      '/local1000': {
+        target: 'http://192.168.2.12:8000'
+      }
+    }
+  }
 })
