7b6eef5a06ad83b03275059e3a2a85060fe1e284
knightingal <yujianming1985@hotmail.com>
2025-03-29 19:49:59 +0800

    test fetch api of local1000

--- src/pages/albumIndex.tsx
+++ src/pages/albumIndex.tsx
@@ -1,6 +1,16 @@
 import DefaultLayout from "@/layouts/default";
+import { useEffect } from "react";
 
 export default function AlbumIndexPage() {
+  useEffect(() => {
+    fetch("/local1000/picIndexAjax").then(resp => {
+      return resp.json()
+    }).then(respJson => {
+      console.log(respJson);
+    })
+  }, [])
+
+
   return <DefaultLayout>
     <h1>AlbumnIndexPage</h1>
   </DefaultLayout>;
