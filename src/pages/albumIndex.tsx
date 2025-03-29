95edcc8e7250b30aaf7d9accf6a95df88d6b7472
knightingal <yujianming1985@hotmail.com>
2025-03-29 21:01:37 +0800

    fetch picIndexAjax interface

--- src/pages/albumIndex.tsx
+++ src/pages/albumIndex.tsx
@@ -1,17 +1,20 @@
 import DefaultLayout from "@/layouts/default";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function AlbumIndexPage() {
+  const [picIndex, setPicIndex] = useState<Array<PicDetail>>([]);
+
   useEffect(() => {
     fetch("/local1000/picIndexAjax").then(resp => {
       return resp.json()
-    }).then(respJson => {
-      console.log(respJson);
+    }).then((respJson: Array<PicDetail>) => {
+      setPicIndex(respJson);
     })
-  }, [])
+  }, []);
 
 
   return <DefaultLayout>
     <h1>AlbumnIndexPage</h1>
+    {picIndex.map((pic => <p key={pic.index}>{pic.name}</p>))}
   </DefaultLayout>;
 }
