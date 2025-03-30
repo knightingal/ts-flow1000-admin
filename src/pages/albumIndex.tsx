a7f02f8f50b4c2addd44aea49af63e7c919219c0
knightingal <yujianming1985@hotmail.com>
2025-03-30 21:47:18 +0800

    import slot classes

--- src/pages/albumIndex.tsx
+++ src/pages/albumIndex.tsx
@@ -4,6 +4,38 @@ import { AlbumConfig, initConfig, PicDetail, RootState, setWindowSize } from "@/
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
+interface SlotItem {
+  index: number,
+  scrollOffset: number,
+  itemHeight: number,
+  slotIndex: number,
+}
+
+class Slot {
+  totalHeight: number = 0;
+  slotItemList: SlotItem[] = [];
+  itemByIndex(index: number): SlotItem|undefined {
+    const got: SlotItem|undefined = this.slotItemList.find(item => {
+      return item.index == index;
+    });
+    return got;
+  }
+}
+
+const minSlot = (slots: Slot[]): number => {
+  let min = slots[0].totalHeight;
+  let index = 0;
+  for (let i = 0; i < 4; i++) {
+    if (slots[i].totalHeight < min) {
+      min = slots[i].totalHeight;
+      index = i;
+    }
+  }
+  return index;
+} 
+
+
+
 const AlbumIndexPage = () => {
   const [picIndex, setPicIndex] = useState<Array<PicDetail>>([]);
   const albumConfigMap = useSelector((state: RootState) => state.flow1000Config.albumConfigMap);
