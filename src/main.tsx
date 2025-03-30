927b9efa12b0b0641279c24d1717f67b5233d67e
knightingal <yujianming1985@hotmail.com>
2025-03-30 11:06:36 +0800

    import redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,20 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.tsx";
 import { Provider } from "./provider.tsx";
+import { Provider as ReduxProvider } from "react-redux";
 import "@/styles/globals.css";
+import { store } from './store'
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
     <BrowserRouter>
-      <Provider>
-        <App />
-      </Provider>
+      <ReduxProvider store={store}>
+        <Provider>
+          <App />
+        </Provider>
+      </ReduxProvider>
     </BrowserRouter>
   // </React.StrictMode>,
 );
