75ec26edab6eaa7b2dcf81a0dc9392e277081bf4
knightingal <yujianming1985@hotmail.com>
2025-04-01 20:24:03 +0800

    switch to `createBrowserRouter`

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import { Route, Routes } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 
 import IndexPage from "@/pages/index";
 import DocsPage from "@/pages/docs";
 import AlbumIndexPage from "@/pages/albumIndex";
 
-function App() {
-  return (
-    <Routes>
-      <Route element={<IndexPage />} path="/" />
-      <Route element={<DocsPage />} path="/docs" />
-      <Route element={<AlbumIndexPage />} path="/albumIndex" />
-    </Routes>
-  );
-}
+export const router = createBrowserRouter([
+  {
+    path:"/", element:<IndexPage />
+  },
+  {
+    path:"/docs", element:<DocsPage />
+  },
+  {
+    path:"/albumIndex", element:<AlbumIndexPage />
+  },
 
-export default App;
+]);
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,13 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 
-import App from "./App.tsx";
-import { Provider } from "./provider.tsx";
+import { router } from "./App.tsx";
 import { Provider as ReduxProvider } from "react-redux";
 import "@/styles/globals.css";
 import { store } from './store'
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // <React.StrictMode>
-    <BrowserRouter>
       <ReduxProvider store={store}>
-        <Provider>
-          <App />
-        </Provider>
+        <RouterProvider router={router} />
       </ReduxProvider>
-    </BrowserRouter>
-  // </React.StrictMode>,
 );
